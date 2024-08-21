176b346e143e0dc75eac6e1492ce48841dc10f4c
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2024-08-20 21:47:14 -0700

    Expanded texture APIs (WIP)

--- common/src/main/java/org/figuramc/figura/model/rendering/texture/FiguraTexture.java
+++ common/src/main/java/org/figuramc/figura/model/rendering/texture/FiguraTexture.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.pipeline.RenderCall;
 import com.mojang.blaze3d.platform.NativeImage;
 import com.mojang.blaze3d.platform.TextureUtil;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.SimpleTexture;
 import net.minecraft.resources.ResourceLocation;
@@ -23,6 +24,9 @@ import org.figuramc.figura.mixin.render.TextureManagerAccessor;
 import org.figuramc.figura.utils.ColorUtils;
 import org.figuramc.figura.utils.FiguraIdentifier;
 import org.figuramc.figura.utils.LuaUtils;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.luaj.vm2.LuaError;
 import org.luaj.vm2.LuaFunction;
 import org.luaj.vm2.LuaValue;
@@ -32,8 +36,11 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.UUID;
+import java.util.function.BiFunction;
 
+@SuppressWarnings("resource")
 @LuaWhitelist
 @LuaTypeDoc(
         name = "Texture",
@@ -56,6 +63,8 @@ public class FiguraTexture extends SimpleTexture {
     private final NativeImage texture;
     private NativeImage backup;
     private boolean isClosed = false;
+
+    private WriteOverflowStrategy writeOverflowStrategy = WriteOverflowStrategy.ERROR;
 
     public FiguraTexture(Avatar owner, String name, byte[] data) {
         super(new FiguraIdentifier("avatar_tex/" + owner.owner + "/" + UUID.randomUUID()));
@@ -92,7 +101,8 @@ public class FiguraTexture extends SimpleTexture {
     }
 
     @Override
-    public void load(ResourceManager manager) throws IOException {}
+    public void load(ResourceManager manager) throws IOException {
+    }
 
     @Override
     public void close() {
@@ -187,6 +197,14 @@ public class FiguraTexture extends SimpleTexture {
         return FiguraVec2.of(getWidth(), getHeight());
     }
 
+    public FiguraVec4 getActualPixel(int x, int y) {
+        try {
+            return ColorUtils.abgrToRGBA(texture.getPixelRGBA(x, y));
+        } catch (Exception e) {
+            throw new LuaError(e.getMessage());
+        }
+    }
+
     @LuaWhitelist
     @LuaMethodDoc(
             overloads = @LuaMethodOverload(
@@ -195,8 +213,23 @@ public class FiguraTexture extends SimpleTexture {
             ),
             value = "texture.get_pixel")
     public FiguraVec4 getPixel(int x, int y) {
+        Pair<Integer, Integer> actual = mapCoordinates(x, y);
+        if (actual == null) throw new LuaError(String.format(
+                "(%d, %d) is out of bounds on %dx%d texture",
+                x, y, getWidth(), getHeight()
+        ));
+        return getActualPixel(actual.getFirst(), actual.getSecond());
+    }
+
+    public FiguraTexture setActualPixel(int x, int y, int color) {
+        return setActualPixel(x, y, color, true);
+    }
+
+    public FiguraTexture setActualPixel(int x, int y, int color, boolean makeBackup) {
         try {
-            return ColorUtils.abgrToRGBA(texture.getPixelRGBA(x, y));
+            if (makeBackup) backupImage();
+            texture.setPixelRGBA(x, y, color);
+            return this;
         } catch (Exception e) {
             throw new LuaError(e.getMessage());
         }
@@ -221,13 +254,10 @@ public class FiguraTexture extends SimpleTexture {
             aliases = "pixel",
             value = "texture.set_pixel")
     public FiguraTexture setPixel(int x, int y, Object r, Double g, Double b, Double a) {
-        try {
-            backupImage();
-            texture.setPixelRGBA(x, y, ColorUtils.rgbaToIntABGR(parseColor("setPixel", r, g, b, a)));
-            return this;
-        } catch (Exception e) {
-            throw new LuaError(e.getMessage());
-        }
+        int color = ColorUtils.rgbaToIntABGR(parseColor("setPixel", r, g, b, a));
+        Pair<Integer, Integer> actual = mapCoordinates(x, y);
+        if (actual == null) return this;
+        return setActualPixel(actual.getFirst(), actual.getSecond(), color);
     }
 
     @LuaWhitelist
@@ -235,6 +265,7 @@ public class FiguraTexture extends SimpleTexture {
         return setPixel(x, y, r, g, b, a);
     }
 
+    @SuppressWarnings("resource")
     @LuaWhitelist
     @LuaMethodDoc(
             overloads = {
@@ -254,8 +285,17 @@ public class FiguraTexture extends SimpleTexture {
             value = "texture.fill")
     public FiguraTexture fill(int x, int y, int width, int height, Object r, Double g, Double b, Double a) {
         try {
+            int color = ColorUtils.rgbaToIntABGR(parseColor("fill", r, g, b, a));
+            // texture.fillRect just does these loops for us, so we can extract them to add the mapping
             backupImage();
-            texture.fillRect(x, y, width, height, ColorUtils.rgbaToIntABGR(parseColor("fill", r, g, b, a)));
+            for (int i = x; i < x + width; i++) {
+                for (int j = y; j < y + height; j++) {
+                    Pair<Integer, Integer> actual = mapCoordinates(i, j);
+                    if (actual == null) continue;
+                    // don't make a copy of the image each time, though
+                    setActualPixel(actual.getFirst(), actual.getSecond(), color, false);
+                }
+            }
             return this;
         } catch (Exception e) {
             throw new LuaError(e.getMessage());
@@ -298,12 +338,23 @@ public class FiguraTexture extends SimpleTexture {
             value = "texture.apply_func"
     )
     public FiguraTexture applyFunc(int x, int y, int width, int height, @LuaNotNil LuaFunction function) {
+        backupImage();
         for (int i = y; i < y + height; i++) {
             for (int j = x; j < x + width; j++) {
-                FiguraVec4 color = getPixel(j, i);
-                LuaValue result = function.call(owner.luaRuntime.typeManager.javaToLua(color).arg1(), LuaValue.valueOf(j), LuaValue.valueOf(i));
-                if (!result.isnil() && result.isuserdata(FiguraVec4.class))
-                    setPixel(j, i, result.checkuserdata(FiguraVec4.class), null, null, null);
+                Pair<Integer, Integer> actual = mapCoordinates(j, i);
+                if (actual == null) continue;
+                int actualX = actual.getFirst(), actualY = actual.getSecond();
+                FiguraVec4 color = getPixel(actualX, actualY);
+                LuaValue result = function.call(
+                        owner.luaRuntime.typeManager.javaToLua(color).arg1(),
+                        LuaValue.valueOf(j),
+                        LuaValue.valueOf(i)
+                );
+                if (!result.isnil() && result.isuserdata(FiguraVec4.class)) {
+                    FiguraVec4 userdata = (FiguraVec4) result.checkuserdata(FiguraVec4.class);
+                    int newColor = ColorUtils.rgbaToIntABGR(userdata);
+                    setActualPixel(actualX, actualY, newColor, false);
+                }
             }
         }
         return this;
@@ -312,28 +363,205 @@ public class FiguraTexture extends SimpleTexture {
     @LuaWhitelist
     @LuaMethodDoc(
             overloads = @LuaMethodOverload(
-                    argumentTypes = {Integer.class, Integer.class, Integer.class, Integer.class, FiguraMat4.class, Boolean.class},
-                    argumentNames = {"x", "y", "width", "height", "matrix", "clip"}
+                    argumentTypes = {Integer.class, Integer.class, Integer.class, Integer.class, FiguraMat4.class},
+                    argumentNames = {"x", "y", "width", "height", "matrix"}
             ),
             value = "texture.apply_matrix"
     )
-    public FiguraTexture applyMatrix(int x, int y, int width, int height, @LuaNotNil FiguraMat4 matrix, boolean clip) {
+    public FiguraTexture applyMatrix(int x,
+                                     int y,
+                                     int width,
+                                     int height,
+                                     @LuaNotNil FiguraMat4 matrix,
+                                     @Nullable Object clip) {
+        // remove next major version
+        if (clip != null) {
+            throw new LuaError(
+                    "texture.applyMatrix's 'clip' argument has been removed (clipping is always enabled)");
+        }
+        backupImage();
         for (int i = y; i < y + height; i++) {
             for (int j = x; j < x + width; j++) {
-                FiguraVec4 color = getPixel(j, i);
+                Pair<Integer, Integer> actual = mapCoordinates(j, i);
+                if (actual == null) continue;
+                int realX = actual.getFirst(), realY = actual.getSecond();
+                FiguraVec4 color = getPixel(realX, realY);
                 color.transform(matrix);
 
-                if (clip) {
-                    color.x = Math.max(0, Math.min(color.x, 1));
-                    color.y = Math.max(0, Math.min(color.y, 1));
-                    color.z = Math.max(0, Math.min(color.z, 1));
-                    color.w = Math.max(0, Math.min(color.w, 1));
-                }
+                color.x = Math.max(0, Math.min(color.x, 1));
+                color.y = Math.max(0, Math.min(color.y, 1));
+                color.z = Math.max(0, Math.min(color.z, 1));
+                color.w = Math.max(0, Math.min(color.w, 1));
 
-                setPixel(j, i, color, null, null, null);
+                setActualPixel(realX, realY, ColorUtils.rgbaToIntABGR(color), false);
             }
         }
         return this;
+    }
+
+    private static final HashMap<String, WriteOverflowStrategy> name2OverflowStrategy = new HashMap<>();
+    private static final HashMap<WriteOverflowStrategy, String> overflowStrategy2Name = new HashMap<>();
+
+    public enum WriteOverflowStrategy {
+        ERROR("error"),
+        DISCARD("ignore", "discard"),
+        WRAP("wrap"),
+        MIRROR("mirror");
+
+        WriteOverflowStrategy(String... names) {
+            for (String name : names)
+                name2OverflowStrategy.put(name, this);
+            if (names.length == 0) throw new IllegalArgumentException("at least one name should be specified");
+            overflowStrategy2Name.put(this, names[0]);
+        }
+    }
+
+    private @Nullable Pair<Integer, Integer> mapCoordinates(int x, int y) throws LuaError {
+        int width = getWidth(), height = getHeight();
+        if (x >= 0 && x < width && y >= 0 && y < height) return Pair.of(x, y);
+        return switch (writeOverflowStrategy) {
+            case ERROR -> throw new LuaError(String.format(
+                    "(%d, %d) is out of bounds on %dx%d texture",
+                    x, y, width, height
+            ));
+            case DISCARD -> null;
+            case WRAP -> Pair.of(
+                    Math.floorMod(x, width),
+                    Math.floorMod(y, height)
+            );
+            case MIRROR -> {
+                // but first, we need to talk about parallel universes
+                int puX = Math.floorDiv(x, width), puY = Math.floorDiv(y, height);
+                // if the original image is PU(0, 0), odd numbered PUs are flipped on one or both axes
+                boolean isXFlipped = Math.floorMod(puX, 2) == 1, isYFlipped = Math.floorMod(puY, 2) == 1;
+                int localX = Math.floorMod(x, width), localY = Math.floorMod(y, height);
+                if (isXFlipped) localX = (width - 1) - localX;
+                if (isYFlipped) localY = (height - 1) - localY;
+                yield Pair.of(localX, localY);
+            }
+        };
+    }
+
+    // Mathematical area operations
+
+    private void assertSameSize(FiguraTexture other) throws LuaError {
+        int otherW = other.getWidth(), otherH = other.getHeight();
+        int thisW = getWidth(), thisH = getHeight();
+        if (thisW != otherW || thisH != otherH) {
+            throw new LuaError(String.format(
+                    "Expected textures to have equal dimensions, but the target is %dx%d and the provided texture is %dx%d",
+                    thisW,
+                    thisH,
+                    otherW,
+                    otherH
+            ));
+        }
+    }
+
+    private static double clamp01(double n) {
+        if (n < 0) return 0;
+        if (n > 1) return 1;
+        return n;
+    }
+
+    private FiguraTexture mathApply(@NotNull FiguraTexture other,
+                                    BiFunction<FiguraVec4, FiguraVec4, FiguraVec4> transform,
+                                    int x,
+                                    int y,
+                                    int w,
+                                    int h) {
+        assertSameSize(other);
+        backupImage();
+        for (int curX = x; curX < x + w; curX++) {
+            for (int curY = y; curY < y + h; curY++) {
+                Pair<Integer, Integer> actualCoordinates = mapCoordinates(curX, curY);
+                if (actualCoordinates == null) continue;
+                int actualX = actualCoordinates.getFirst(), actualY = actualCoordinates.getSecond();
+                try {
+                    FiguraVec4 colorA = ColorUtils.abgrToRGBA(texture.getPixelRGBA(actualX, actualY));
+                    FiguraVec4 colorB = ColorUtils.abgrToRGBA(other.texture.getPixelRGBA(actualX, actualY));
+                    FiguraVec4 result = transform.apply(colorA, colorB);
+                    result = FiguraVec4.of(
+                            clamp01(result.x),
+                            clamp01(result.y),
+                            clamp01(result.z),
+                            clamp01(result.w)
+                    );
+                    texture.setPixelRGBA(actualX, actualY, ColorUtils.rgbaToIntABGR(result));
+                } catch (Exception e) {
+                    restore();
+                    if (curX != actualX || curY != actualY)
+                        throw new LuaError(String.format(
+                                "While applying pixel at actual(%d, %d) / virtual(%d, %d): %s",
+                                actualX, actualY,
+                                curX, curY,
+                                e.getMessage()
+                        ));
+                    throw new LuaError(String.format(
+                            "While applying pixel at (%d, %d): %s",
+                            actualX, actualY, e.getMessage()
+                    ));
+                }
+            }
+        }
+        return this;
+    }
+
+    private static final BiFunction<FiguraVec4, FiguraVec4, FiguraVec4> opMultiply = FiguraVec4::times;
+    private static final BiFunction<FiguraVec4, FiguraVec4, FiguraVec4> opDivide = FiguraVec4::dividedBy;
+    private static final BiFunction<FiguraVec4, FiguraVec4, FiguraVec4> opAdd = FiguraVec4::plus;
+    private static final BiFunction<FiguraVec4, FiguraVec4, FiguraVec4> opSubtract = FiguraVec4::minus;
+
+    private FiguraTexture mathFunction(@NotNull FiguraTexture other,
+                                       int x,
+                                       int y,
+                                       int w,
+                                       int h,
+                                       BiFunction<FiguraVec4, FiguraVec4, FiguraVec4> transform) {
+        return mathApply(other, transform, x, y, w, h);
+    }
+
+    @LuaWhitelist
+    public FiguraTexture multiply(@LuaNotNil @NotNull FiguraTexture other, int x, int y, int w, int h) {
+        return mathFunction(other, x, y, w, h, opMultiply);
+    }
+
+    @LuaWhitelist
+    public FiguraTexture divide(@LuaNotNil @NotNull FiguraTexture other, int x, int y, int w, int h) {
+        return mathFunction(other, x, y, w, h, opDivide);
+    }
+
+    @LuaWhitelist
+    public FiguraTexture add(@LuaNotNil @NotNull FiguraTexture other, int x, int y, int w, int h) {
+        return mathFunction(other, x, y, w, h, opAdd);
+    }
+
+    @LuaWhitelist
+    public FiguraTexture subtract(@LuaNotNil @NotNull FiguraTexture other, int x, int y, int w, int h) {
+        return mathFunction(other, x, y, w, h, opSubtract);
+    }
+
+    @LuaWhitelist
+    public FiguraTexture setOverflowMode(@LuaNotNil @NotNull String mode) {
+        if (!name2OverflowStrategy.containsKey(mode)) {
+            int i = 0;
+            StringBuilder options = new StringBuilder();
+            for (String k : name2OverflowStrategy.keySet()) {
+                if (i++ > 0) options.append(", ");
+                options.append("'").append(k).append("'");
+            }
+            throw new LuaError(String.format(
+                    "Unknown wrapping mode '%s'\n(known: " + options + ")",
+                    mode
+            ));
+        }
+        writeOverflowStrategy = name2OverflowStrategy.get(mode);
+        return this;
+    }
+    
+    @LuaWhitelist
+    public String getOverflowMode() {
+        return overflowStrategy2Name.get(writeOverflowStrategy);
     }
 
     @LuaWhitelist
