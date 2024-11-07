ed387ab56ae164507026c668d5827f7b46f244cc
Lexize <lexizej@gmail.com>
2024-11-07 23:36:59 +0500

    1.20.2 port

--- neoforge/src/main/java/org/figuramc/figura/neoforge/FiguraServerForge.java
+++ neoforge/src/main/java/org/figuramc/figura/neoforge/FiguraServerForge.java
@@ -8,6 +8,8 @@ import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.neoforge.network.INetworkDirection;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.PlayNetworkDirection;
+import net.neoforged.neoforge.server.permission.PermissionAPI;
+import net.neoforged.neoforge.server.permission.nodes.PermissionNode;
 import org.figuramc.figura.backend2.ForgeNetworking;
 import org.figuramc.figura.server.FiguraModServer;
 import org.figuramc.figura.server.PayloadWrapper;
@@ -25,5 +27,12 @@ public class FiguraServerForge extends FiguraModServer {
         var channel = ForgeNetworking.getChannel(id);
         if (channel == null) return;
         PacketDistributor.PLAYER.with(() -> player).send(new ClientboundCustomPayloadPacket(new PayloadWrapper(packet)));
+    }
+
+    @Override
+    public boolean getPermission(UUID player, String permission) {
+        ServerPlayer pl = getServer().getPlayerList().getPlayer(player);
+        PermissionNode<Boolean> perm = FiguraForgePermissions.getPermission(permission);
+        return pl != null && perm != null && PermissionAPI.getPermission(pl, perm);
     }
 }
