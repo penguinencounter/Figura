274771946c35a0c7764809513f399d7888c42119
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-02-16 20:29:11 -0600

    Fix a possible minor issue with post effects

--- common/src/main/java/org/figuramc/figura/lua/api/RendererAPI.java
+++ common/src/main/java/org/figuramc/figura/lua/api/RendererAPI.java
@@ -429,8 +429,10 @@ public class RendererAPI {
     )
     public RendererAPI setPostEffect(String effect) {
         this.postShader = effect == null || effect.isBlank() ? null : LuaUtils.parsePath(effect);
-        if (postShader != null && Minecraft.getInstance().getResourceManager().getResource(LuaUtils.parsePath("post_effect/" + effect + ".json")).isEmpty())
+        if (postShader != null && Minecraft.getInstance().getResourceManager().getResource(LuaUtils.parsePath("post_effect/" + effect + ".json")).isEmpty()) {
+            postShader = null;
             throw new LuaError("The post shader %s does not exist or could not be found".formatted(postShader.toString()));
+        }
         return this;
     }
 
