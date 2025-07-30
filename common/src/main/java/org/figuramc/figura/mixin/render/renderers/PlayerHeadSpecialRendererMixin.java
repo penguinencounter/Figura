532fcb53830ed067105ec45a7312026e6c0c49e3
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-07-30 19:55:43 +0530

    Fix skull model not updating in inventory

--- common/src/main/java/org/figuramc/figura/mixin/render/renderers/PlayerHeadSpecialRendererMixin.java
+++ common/src/main/java/org/figuramc/figura/mixin/render/renderers/PlayerHeadSpecialRendererMixin.java
@@ -1,42 +1,16 @@
 package org.figuramc.figura.mixin.render.renderers;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.culling.Frustum;
-import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.client.renderer.special.PlayerHeadSpecialRenderer;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ResolvableProfile;
-import org.figuramc.figura.FiguraMod;
 import org.figuramc.figura.avatar.Avatar;
 import org.figuramc.figura.avatar.AvatarManager;
-import org.figuramc.figura.config.Configs;
-import org.figuramc.figura.ducks.EntityRendererAccessor;
-import org.figuramc.figura.ducks.FiguraEntityRenderStateExtension;
-import org.figuramc.figura.ducks.SkullBlockRendererAccessor;
 import org.figuramc.figura.ducks.SkullBlockRendererHelper;
-import org.figuramc.figura.lua.api.nameplate.EntityNameplateCustomization;
-import org.figuramc.figura.math.vector.FiguraVec3;
-import org.figuramc.figura.permissions.Permissions;
-import org.figuramc.figura.utils.TextUtils;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.List;
 
 @Mixin(PlayerHeadSpecialRenderer.class)
 public abstract class PlayerHeadSpecialRendererMixin {
---
+++ common/src/main/java/org/figuramc/figura/mixin/render/renderers/SpecialModelWrapperMixin.java
@@ -0,0 +1,31 @@
+package org.figuramc.figura.mixin.render.renderers;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
+import net.minecraft.client.renderer.item.SpecialModelWrapper;
+import net.minecraft.client.renderer.special.PlayerHeadSpecialRenderer;
+import net.minecraft.client.renderer.special.SpecialModelRenderer;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ResolvableProfile;
+import org.figuramc.figura.avatar.Avatar;
+import org.figuramc.figura.avatar.AvatarManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SpecialModelWrapper.class)
+public class SpecialModelWrapperMixin<T> {
+    @Shadow @Final private SpecialModelRenderer<T> specialRenderer;
+
+    @Inject(method = "update", at = @At(value = "RETURN"))
+    public void update(CallbackInfo ci, @Local(argsOnly = true) ItemStackRenderState layerRenderState, @Local(argsOnly = true)ItemStack stack) {
+        ResolvableProfile profile = stack.get(DataComponents.PROFILE);
+        Avatar avatar = (profile != null && profile.gameProfile() != null) ? AvatarManager.getAvatarForPlayer(profile.gameProfile().getId()) : null;
+        if (avatar != null && specialRenderer instanceof PlayerHeadSpecialRenderer)
+            layerRenderState.setAnimated();
+    }
+}
