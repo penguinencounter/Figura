0844097b87d7fbcb58329e8a9a2ead4630ac9368
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2026-01-08 10:46:02 -0800

    block uploading from Figura lua namespace

--- common/src/main/java/org/figuramc/figura/commands/UploadCommand.java
+++ common/src/main/java/org/figuramc/figura/commands/UploadCommand.java
@@ -2,17 +2,34 @@ package org.figuramc.figura.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
+import net.minecraft.network.chat.Component;
 import org.figuramc.figura.FiguraMod;
 import org.figuramc.figura.avatar.Avatar;
 import org.figuramc.figura.avatar.AvatarManager;
 import org.figuramc.figura.backend2.NetworkStuff;
 import org.figuramc.figura.gui.widgets.lists.AvatarList;
+import org.figuramc.figura.lua.LuaTypeManager;
 import org.figuramc.figura.utils.FiguraClientCommandSource;
+
+import java.util.Set;
 
 class UploadCommand {
     public static LiteralArgumentBuilder<FiguraClientCommandSource> getCommand() {
         LiteralArgumentBuilder<FiguraClientCommandSource> upload = LiteralArgumentBuilder.literal("upload");
         upload.executes(context -> {
+            // Check stack frames
+            StackTraceElement[] frames = Thread.currentThread().getStackTrace();
+            int i = 0;
+            for (StackTraceElement frame : frames) {
+                if (frame.getClassName().startsWith("org.figuramc.figura.lua")) {
+                    context.getSource().figura$sendError(
+                            Component.literal("Uploading avatars using Figura scripts is not supported.")
+                    );
+                    return 0;
+                }
+                FiguraMod.LOGGER.info("frame {} : {} {} {}:{}", i++, frame.getClassName(), frame.getMethodName(), frame.getFileName(), frame.getLineNumber());
+            }
+
             Avatar avatar = AvatarManager.getAvatarForPlayer(FiguraMod.getLocalPlayerUUID());
 
             NetworkStuff.uploadAvatar(avatar);
